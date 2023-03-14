34c8cc16561160a91c24dc950ba9d731f0bdecde
kbogar <krisztianbogar@gmail.com>
2023-03-14 13:47:04 +0000

    add empty quote to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -17,7 +17,7 @@ import dj_database_url
 if os.path.exists('env.py'):
     import env
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
